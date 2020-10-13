8108857d53fb75a684a4daadbd3d2661ff07a451
Chinmay Kurade <chinmay.v.kurade@gmail.com>
2020-10-13 21:45:29 +0530

    update

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ app = Flask(__name__)
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://second:second@localhost:3306/hello'
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://qdmvuwwwauegyn:cf24a691eca9f2c73caa3da50a89c63386a9bc9fcc4ab9faa9f249aaba04fc34@ec2-3-215-207-12.compute-1.amazonaws.com:5432/d4e20ubj61kkhn'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 class Todo(db.Model):

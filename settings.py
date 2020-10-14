ed395a97474ac6f3b4c435a9d960b7a11e05187d
Chinmay Kurade <chinmay.v.kurade@gmail.com>
2020-10-14 07:12:53 +0530

    change

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
-# SQLALCHEMY_DATABASE_URI = 'postgres://qdmvuwwwauegyn:cf24a691eca9f2c73caa3da50a89c63386a9bc9fcc4ab9faa9f249aaba04fc34@ec2-3-215-207-12.compute-1.amazonaws.com:5432/d4e20ubj61kkhn'
+SQLALCHEMY_DATABASE_URI = 'postgres://qdmvuwwwauegyn:cf24a691eca9f2c73caa3da50a89c63386a9bc9fcc4ab9faa9f249aaba04fc34@ec2-3-215-207-12.compute-1.amazonaws.com:5432/d4e20ubj61kkhn'
 # SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://second:second@localhost:3306/hello'
-SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
+# SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
 SECRET_KEY = 'tm01test'
 SQLALCHEMY_TRACK_MODIFICATIONS = False

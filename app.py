ee7749430c01b61fc6aca009ef8c62dfcdb6a099
Chinmay Kurade <chinmay.v.kurade@gmail.com>
2020-10-14 07:10:49 +0530

    add

--- app.py
+++ app.py
@@ -10,10 +10,7 @@ from datetime import datetime
 
 # app = Flask(__name__) # to make the app run without any
 app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://second:second@localhost:3306/hello'
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://qdmvuwwwauegyn:cf24a691eca9f2c73caa3da50a89c63386a9bc9fcc4ab9faa9f249aaba04fc34@ec2-3-215-207-12.compute-1.amazonaws.com:5432/d4e20ubj61kkhn'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config.from_pyfile('settings.py')
 db = SQLAlchemy(app)
 
 class Todo(db.Model):
